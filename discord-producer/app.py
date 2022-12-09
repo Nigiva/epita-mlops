7a6000b646f498584b5e71d1abe351778b7e412e
Corentin Duchêne <corentin.duchene@epita.fr>
2022-12-10 00:45:40 +0100

    feat(discord-producer): add message producer

--- discord-producer/app.py
+++ discord-producer/app.py
@@ -2,7 +2,9 @@ from dotenv import load_dotenv
 from loguru import logger
 import os
 import discord
-from logger import intercept_discord_logging
+from logger import intercept_logging
+from kafka import KafkaProducer
+import json
 
 logger.info("Starting Discord Producer")
 
@@ -16,10 +18,13 @@ else:
 # Get environment variables
 LOG_PATH = os.getenv("LOG_PATH")
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "discordmessage")
 
 # Set up logging
 logger.add(LOG_PATH, rotation="1 day", retention="1 month", level="DEBUG")
-intercept_discord_logging(logger)
+intercept_logging("discord", logger)
+intercept_logging("kafka", logger)
 
 # Check the presence of the Discord token
 if DISCORD_TOKEN is None or DISCORD_TOKEN == "":
@@ -32,6 +37,9 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+# Set up Kafka producer
+producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER)
+
 # Event handler for when the bot is ready
 @client.event
 async def on_ready():
@@ -43,7 +51,14 @@ async def on_message(message):
         return
     
     logger.debug(f"Received message from {message.author}: {message.content}")
+    message_object = {
+        "id": message.id,
+        "content": message.content,
+    }
+    message_json = json.dumps(message_object)
+    producer.send(KAFKA_TOPIC, message_json.encode("utf-8"))
+    
     await message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
-    await message.add_reaction("\N{NO ENTRY}")
+    # await message.add_reaction("\N{NO ENTRY}")
     
 client.run(DISCORD_TOKEN)
