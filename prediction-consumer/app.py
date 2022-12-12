b7e15df664fb2e1bb4e0352cb2ea69646ca70408
Corentin Duchêne <corentin.duchene@epita.fr>
2022-12-12 01:23:45 +0100

    build(prediction-consumer): dockerize prediction-consumer

--- prediction-consumer/app.py
+++ prediction-consumer/app.py
@@ -2,11 +2,11 @@ from dotenv import load_dotenv
 from loguru import logger
 import os
 import discord
-import time
 from logger import intercept_logging
 from kafka import KafkaConsumer
+import time
 import json
-import asyncio
+import logging
 
 logger.info("Starting Prediction Consumer")
 
@@ -31,12 +31,16 @@ else:
 
 KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
 logger.info(f"Kafka broker: {KAFKA_BROKER}")
-KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "discordmessage")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "modelprediction")
 logger.info(f"Kafka topic: {KAFKA_TOPIC}")
+DEBUG_MODE = os.getenv("DEBUG_MODE", "False") == "True"
+logger.info(f"Debug mode: {DEBUG_MODE}")
+WAIT_FOR_KAFKA = int(os.getenv("WAIT_FOR_KAFKA", 10))
+logger.info(f"Wait for Kafka (secondes): {WAIT_FOR_KAFKA}")
 
 # Set up logging
 logger.add(LOG_PATH, rotation="1 day", retention="1 month", level="DEBUG")
-intercept_logging("discord", logger)
+intercept_logging("discord", logger, level=logging.ERROR)
 intercept_logging("kafka", logger)
 
 # Set up Discord intents
@@ -49,36 +53,61 @@ client = discord.AutoShardedClient(
 )
 
 # Set up Kafka consumer
+logger.info("Waiting for Kafka broker to be ready")
+time.sleep(WAIT_FOR_KAFKA)
+
 consumer = KafkaConsumer(
     bootstrap_servers=KAFKA_BROKER,
     group_id="prediction-consumer",
 )
 consumer.subscribe(topics=[KAFKA_TOPIC])
 
-async def process_message(channel_id, message_id):
-    logger.debug(f"Process message with discord {message_id=} {channel_id=}")
+async def process_discord_message(channel_id, message_id, is_toxic):
+    logger.debug(f"Process discord message {message_id=} {channel_id=}")
+    logger.debug(f"Getting Discord Channels: {channel_id=}")
     discord_channel = client.get_channel(channel_id)
     if discord_channel is None:
         logger.error(f"Discord channel {channel_id} not found")
         return
+    
     try:
+        logger.debug(f"Fetching Discord Message: {message_id=}")
         discord_message = await discord_channel.fetch_message(message_id)
-        await discord_message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
-        # await discord_message.add_reaction("\N{NO ENTRY}")
     except discord.errors.NotFound:
         logger.error(f"Discord message {message_id} not found")
+        return
 
+    if not DEBUG_MODE and is_toxic:
+        logger.info(f"Message {message_id=} is toxic")
+        logger.debug(f"Deleting Discord Message {message_id=}")
+        await discord_message.delete()
+        return
+    
+    if DEBUG_MODE:
+        if is_toxic:
+            logger.info(f"Message {message_id=} is toxic")
+            logger.debug(f"Add reaction to Discord Message {message_id=}")
+            await discord_message.add_reaction("\N{NO ENTRY}")
+        else:
+            logger.info(f"Message {message_id=} is not toxic")
+            logger.debug(f"Add reaction to Discord Message {message_id=}")
+            await discord_message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
+            
+            
+        
+        
 @client.event
 async def on_ready():
-    for message in consumer:
-        decoded_message = message.value.decode("utf-8")
-        message_obj = json.loads(decoded_message)
+    logger.success("Prediction Consumer is ready !")
+    for prediction in consumer:
+        prediction_str = prediction.value.decode("utf-8")
+        prediction_obj = json.loads(prediction_str)
         
-        channel_id = message_obj["channel_id"]
-        message_id = message_obj["message_id"]
-        message_content = message_obj["message_content"]
+        channel_id = prediction_obj["channel_id"]
+        message_id = prediction_obj["message_id"]
+        is_toxic = prediction_obj["is_toxic"]
         
-        logger.debug(f"Received message {message_id=}")
-        await process_message(channel_id, message_id)
+        logger.debug(f"Received prediction {message_id=} {channel_id=} {is_toxic=}")
+        await process_discord_message(channel_id, message_id, is_toxic)
 
 client.run(DISCORD_TOKEN)
